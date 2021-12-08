b4c185e7e46e97d8c85c87f5004de1df774bbf92
Muhammad <muhammadhullah23@gmail.com>
2021-12-08 04:36:21 -0500

    import rootReducer function in index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { rootReducer } from './reducers/rootReducer'
 
-let store = createStore(rootreducer, applyMiddleware(thunk))
+let store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
