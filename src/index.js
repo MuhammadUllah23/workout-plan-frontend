2705aed9a1d2836ce33c87c302c91c819edb3364
Muhammad <muhammadhullah23@gmail.com>
2021-12-08 03:31:18 -0500

    import thunk from redux-thunk, and createStore & applyMiddleware from reducx

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(
   <React.StrictMode>
