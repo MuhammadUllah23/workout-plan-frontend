6be2e93390e57d4fb48f956256e4ab5669e7439c
Muhammad <muhammadhullah23@gmail.com>
2021-12-08 04:31:09 -0500

    define rootReducer which combines workoutReducer and exerciseReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
 import workoutReducer from "./workoutReducer";
 import exerciseReducer from "./exerciseReducer";
+
+export const rootReducer = combineReducers({
+    workouts: workoutReducer,
+    exercises: exerciseReducer
+})
