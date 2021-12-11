5f54fdcd4424d2e7ebcae00e347a89274d596891
Muhammad <muhammadhullah23@gmail.com>
2021-12-10 19:40:50 -0500

    Route in app.js to WorkoutList Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import react from 'react';
-
+import WorkoutList from './components/WorkoutList';
 
 function App() {
   return (
     <div className="App">
-     
+    <h1>Welcome</h1>
+    <Routes>
+      <Route path="/workouts" element={<WorkoutList/>}/>
+    </Routes>
     </div>
   );
 }
