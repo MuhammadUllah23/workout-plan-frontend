43e3f944e7d0299ef3a1c3bed705ff908818adf9
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 18:32:19 -0500

    import useSelector and useDispatch from react-redux in WorkoutList.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import './App.css';
-// import WorkoutList from './components/WorkoutList';
+import WorkoutList from './components/WorkoutList';
 
 function App() {
   return (
     <div className="App">
     <h1>Welcome</h1>
-    {/* <Link to="/">Home</Link> */}
-    {/* <Link to="/workouts">Workouts</Link> */}
-    {/* <Routes>
+    <Link to="/">Home</Link>
+    <Link to="/workouts">Workouts</Link>
+    <Routes>
       <Route path="/workouts" element={<WorkoutList/>}/>
-    </Routes> */}
+    </Routes>
     </div>
   );
 }
