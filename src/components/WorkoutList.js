d3a44058d5b2a705eab11b7d0fb8c09003622ceb
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 18:41:05 -0500

    useSelector as mapStateToProps and useDispatch to dispatch action

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function WorkoutList() {
+    const workouts = useSelector(state => state.workouts)
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Workouts</h2>
