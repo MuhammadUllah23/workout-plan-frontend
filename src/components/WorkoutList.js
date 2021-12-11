5338cdf69b3f29ba435a92987d2302a53d4251af
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 18:47:09 -0500

    change port to start at 3001

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,9 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { fetchWorkouts } from "../actions/WorkoutActions";
 
 export default function WorkoutList() {
     const workouts = useSelector(state => state.workouts)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchWorkouts())
+    }, [])
     return (
         <div>
             <h2>Workouts</h2>
