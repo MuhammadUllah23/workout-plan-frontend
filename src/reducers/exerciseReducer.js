6c1ad5248bb6e0ab0b63a08e60d20332ac282af7
Muhammad <muhammadhullah23@gmail.com>
2021-12-12 05:31:50 -0500

    DELETE_EXERCISE reducer

--- src/reducers/exerciseReducer.js
+++ src/reducers/exerciseReducer.js
@@ -4,8 +4,11 @@ export default function exerciseReducer(state = [], action){
         case "SET_EXERCISES":
             return action.payload
 
-            case "ADD_EXERCISE":
-                return [...state, action.payload];
+        case "ADD_EXERCISE":
+            return [...state, action.payload];
+                
+        case "DELETE_EXERCISE":
+            return state.filter(exercise => exercise.id !== action.payload)
 
         default: 
         return state
