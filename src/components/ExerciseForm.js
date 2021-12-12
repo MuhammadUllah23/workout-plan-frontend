44d692f2a6c72a8160cf46e60dd784238c848d20
Muhammad <muhammadhullah23@gmail.com>
2021-12-12 05:13:58 -0500

    can create new exercise for workout

--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -2,14 +2,70 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from 'react-router-dom'
 import { useNavigate } from "react-router";
+import { createExercise } from "../actions/ExerciseActions";
 
 
 export default function ExerciseForm({}) {
     const location = useLocation()
     const { workoutId } = location.state
-    debugger
+    
+    const [exercise, setExercise] = useState({
+        name: "",
+        target: "",
+        sets: "",
+        reps: "",
+        workout_id: workoutId
+    })
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    function handleSubmit(e){
+       e.preventDefault()
+       
+       dispatch(createExercise({exercise}, navigate))
+    }
+
     return (
-        <p>hi</p>
+        <form onSubmit={handleSubmit}>
+        <h2>Create Workout</h2>
+           <input
+           type="text"
+           onChange={e => setExercise({...exercise, name: e.target.value})}
+           name="name"
+           id="name"
+           value={exercise.name}
+           placeholder="Exercise Name" />
+
+           <input
+           type="text"
+           onChange={e => setExercise({...exercise, target: e.target.value})}
+           name="target"
+           id="target"
+           value={exercise.target}
+           placeholder="Target Areas" />
+
+           <input
+           type="number"
+           min='1'
+           onChange={e => setExercise({...exercise, sets: e.target.value})}
+           name="sets"
+           id="sets"
+           value={exercise.sets}
+           placeholder="Sets" />
+
+           <input
+           type="number"
+           min='1'
+           onChange={e => setExercise({...exercise, reps: e.target.value})}
+           name="reps"
+           id="reps"
+           value={exercise.reps}
+           placeholder="Reps" />
+           
+           <input type="submit" />
+        </form>  
+        
     )
 
 }
