92dd332fb13e83f3f0c0848490586f740180d3ea
Muhammad <muhammadhullah23@gmail.com>
2021-12-12 01:18:14 -0500

    1. Set WorkoutList function to a variable that takes in workouts argument. 2. Implemented a 'view Workout' link that takes user to the specific workout.

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,14 +1,32 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchWorkouts } from "../actions/WorkoutActions";
+// // import { useEffect } from "react";
+// // import { useSelector, useDispatch } from "react-redux";
+// // import { fetchWorkouts } from "../actions/WorkoutActions";
 
-export default function WorkoutList() {
-    const workouts = useSelector(state => state.workouts)
-    const dispatch = useDispatch()
+// export default function WorkoutList({workouts}) {
+//     // const workouts = useSelector(state => state.workouts)
+//     // const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(fetchWorkouts())
-    }, [])
+//     // useEffect(() => {
+//     //     dispatch(fetchWorkouts())
+//     // }, [])
+//     debugger
+//     return (
+//         <div>
+//             <h2>Workouts</h2>
+//             <ul>
+//                 {workouts.map(n => 
+//                 <li key={n.id}>
+//                 <h4>{n.title}</h4> 
+//                 <p>Focus: {n.focus}</p>
+//                 <br></br>
+//                 </li>)}
+//             </ul>
+//         </div>
+//     )
+// }
+import {Link} from 'react-router-dom'
+
+const WorkoutList = ({ workouts }) => {
     return (
         <div>
             <h2>Workouts</h2>
@@ -17,9 +35,12 @@ export default function WorkoutList() {
                 <li key={n.id}>
                 <h4>{n.title}</h4> 
                 <p>Focus: {n.focus}</p>
+                <p><Link to={`/workout/${n.id}`}>View Workout</Link></p>
                 <br></br>
                 </li>)}
             </ul>
         </div>
     )
-}
+  };
+  
+  export default WorkoutList;
