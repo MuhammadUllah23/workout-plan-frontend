6f4cb1d7b596c6d1ff175d7e9a5e7d0ec9b238a8
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 21:19:34 -0500

    Input 'ADD_NOTE' reducer in WorkoutReducer.js

--- src/reducers/workoutReducer.js
+++ src/reducers/workoutReducer.js
@@ -2,6 +2,10 @@ export default function workoutReducer(state = [], action){
     switch (action.type) {
         case "SET_WORKOUT":
             return action.payload
+
+            case "ADD_NOTE":
+                return [...state, action.payload];
+                
         default: 
         return state
     }
