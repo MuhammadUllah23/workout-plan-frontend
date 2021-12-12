d7f2ad3bc21477463580d6a4df28786d8a904b26
Muhammad <muhammadhullah23@gmail.com>
2021-12-11 19:54:30 -0500

    build form to typr in title and focus

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -2,11 +2,27 @@
 
  export default function WorkoutForm() {
      const [workout, setWorkout] = useState("")
-     
+    console.log(workout.title)
      return (
          <form>
-             <h2>Create Workout</h2>
-             <input type="submit" />
-         </form>
+         <h2>Create Workout</h2>
+            <input
+            type="text"
+            onChange={e => setWorkout(e.target.value)}
+            name="title"
+            value={workout.title}
+            placeholder="Title" />
+
+            <input
+            type="text"
+            onChange={e => setWorkout(e.target.value)}
+            name="focus"
+            value={workout.focus}
+            placeholder="Focus" />
+            
+            <input type="submit" />
+         </form>  
+             
+         
      )
  }
