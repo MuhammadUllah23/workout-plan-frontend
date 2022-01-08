7e3ce964fd8a19babc0f1ec151e9a36275ee1585
Muhammad <muhammadhullah23@gmail.com>
2022-01-07 23:27:45 -0500

    fetch action to create user
    Does not have dispatch to reducer

---
+++ src/actions/UserActions.js
@@ -0,0 +1,14 @@
+export function createUser(user) {
+    return dispatch =>{
+        fetch("http://localhost:3000/api/v1/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(user),
+})
+  .then((r) => r.json())
+  .then(console.log);
+    }
+}
