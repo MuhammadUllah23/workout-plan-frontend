3646a039792453d69754556bcd7889260f7ee7d0
Muhammad <muhammadhullah23@gmail.com>
2022-01-07 23:47:57 -0500

    set user initial state

--- src/components/user/Signup.js
+++ src/components/user/Signup.js
@@ -4,6 +4,13 @@ import { createUser } from "../../actions/UserActions"
 import { useNavigate } from "react-router-dom"
 
 export default function Signup() {
+    const [user, setUser] = useState({
+        email: "",
+        username: "",
+        password: "",
+        password_confirmation: ""
+    })
+    
     return (
         <p>Signup</p>
     )
