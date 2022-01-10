53926e5ef7f6b6dcd97561eb91e892338ecd6a04
Muhammad <muhammadhullah23@gmail.com>
2022-01-10 07:58:13 -0500

    create login form
    User needs to enter email and password

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 export default function login({ onLogin }) {
+    const [user, setUser] = useState("");
     return (
-        <p>login</p>
+        <div>
+            <h2>Login</h2>
+            <form className="form" onSubmit={handleSubmit}>
+        
+            <input 
+            type="email"
+            onChange={e => setUser({...user, email: e.target.value})}
+            name="email"
+            id="email"
+            value={user.email}
+            placeholder="Email"
+            />
+            <br />
+
+            <input 
+            type="password"
+            onChange={e => setUser({...user, password: e.target.value})}
+            name="password"
+            id="password"
+            value={user.password}
+            placeholder="Password"
+            />
+            <br />
+
+            <input className="submitInput" type="submit" />
+            </form>
+        </div>
     )
 }
