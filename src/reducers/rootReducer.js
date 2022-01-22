40d83f0de98752179016743b166cfe4fddda4fd2
Muhammad <muhammadhullah23@gmail.com>
2022-01-22 12:33:27 -0500

    Import userReducer to combineReducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import workoutReducer from "./workoutReducer";
 import exerciseReducer from "./exerciseReducer";
+import userReducer from "./userReducer";
 
 export const rootReducer = combineReducers({
     workouts: workoutReducer,
-    exercises: exerciseReducer
+    exercises: exerciseReducer,
+    users: userReducer
 })
