fdf9dfcd6a1e03e104b0f7f9c897b8b0bdf7daa4
Muhammad <muhammadhullah23@gmail.com>
2022-01-25 12:44:32 -0500

    UPDATE_EXERCISE Reducer

--- src/reducers/exerciseReducer.js
+++ src/reducers/exerciseReducer.js
@@ -1,4 +1,4 @@
-import { SET_EXERCISES, ADD_EXERCISE, DELETE_EXERCISE } from "../actions/constants";
+import { SET_EXERCISES, ADD_EXERCISE, DELETE_EXERCISE, UPDATE_EXERCISE } from "../actions/constants";
 
 export default function exerciseReducer(state = [], action){
     switch (action.type) {
@@ -7,6 +7,10 @@ export default function exerciseReducer(state = [], action){
 
         case ADD_EXERCISE:
             return [...state, action.payload];
+        
+        case UPDATE_EXERCISE:
+            const newStateArray = state.filter((exercise) => exercise !== action.payload)
+            return [...newStateArray, action.paylaod];
                 
         case DELETE_EXERCISE:
             return state.filter(exercise => exercise.id !== action.payload)
