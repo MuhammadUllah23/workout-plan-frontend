3f236dc0d637bd15d3f4d1ac675700ff9ddfacfc
Muhammad <muhammadhullah23@gmail.com>
2022-01-25 12:46:51 -0500

    UPDATE_WORKOUT Reducer

--- src/reducers/workoutReducer.js
+++ src/reducers/workoutReducer.js
@@ -1,4 +1,4 @@
-import { SET_WORKOUT, ADD_WORKOUT, DELETE_WORKOUT } from "../actions/constants";
+import { SET_WORKOUT, ADD_WORKOUT, DELETE_WORKOUT, UPDATE_WORKOUT } from "../actions/constants";
 
 export default function workoutReducer(state = [], action){
     switch (action.type) {
@@ -7,6 +7,10 @@ export default function workoutReducer(state = [], action){
 
         case ADD_WORKOUT:
             return [...state, action.payload];
+
+        case UPDATE_WORKOUT:
+            const newStateArray = state.filter((workout) => workout !== action.payload)
+            return [...newStateArray, action.payload];
 
         case DELETE_WORKOUT:
             return state.filter(workout => workout.id !== action.payload)
