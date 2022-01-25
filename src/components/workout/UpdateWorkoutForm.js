6ddabd31cfea08916fe198480cc9f2cf419a538b
Muhammad <muhammadhullah23@gmail.com>
2022-01-25 12:54:16 -0500

    Import files in UpdateWorkoutForm

--- src/components/workout/UpdateWorkoutForm.js
+++ src/components/workout/UpdateWorkoutForm.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { useNavigate } from "react-router";
+import { updateWorkout } from "../../actions/WorkoutActions";
+import { useParams } from "react-router"
+
 export default function UpdateWorkoutForm() {
+
+    const { id } = useParams()
+
+    const [workout, setWorkout] = useState({
+        title: "",
+        focus: "",
+        id: ""
+    })
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
     return (
         <div>
             <p>Update Workout Form</p>
