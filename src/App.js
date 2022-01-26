e83f1341133c0e6866125374cf6f3d574ea45fa9
Muhammad <muhammadhullah23@gmail.com>
2022-01-26 11:18:43 -0500

    UpdateWorkoutForm Route path

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { useEffect } from "react";
 import { useDispatch } from 'react-redux';
-import { fetchWorkouts } from './actions/WorkoutActions';
+import { fetchWorkouts, updateWorkout } from './actions/WorkoutActions';
 import { fetchExercises } from './actions/ExerciseActions';
 import { fetchUsers } from './actions/UserActions'; 
 
 import ExerciseForm from './components/exercise/ExerciseForm';
 import ExerciseList from './components/exercise/ExerciseList';
 import WorkoutForm from './components/workout/WorkoutForm';
+import UpdateWorkoutForm from './components/workout/UpdateWorkoutForm';
 import WorkoutList from './components/workout/WorkoutList'
 import WorkoutShow from './components/workout/WorkoutShow'
 import Home from './components/Home';
@@ -37,6 +38,7 @@ function App() {
             <Route path="/workouts" element={<WorkoutList />}/>
             <Route path="/workouts/new" element={<WorkoutForm/>}/>
             <Route path="/workout/:id" element={<WorkoutShow />} />
+            <Route path="/workout/:id/edit" element={<UpdateWorkoutForm />} />
             <Route path="/exercises" element={<ExerciseList  />}/>
             <Route path="/workout/:id/exercises/new" element={<ExerciseForm />}/>
             <Route path="/" element={<Home />} />
